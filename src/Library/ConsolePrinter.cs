098d9bb055b0e74d4933bef9a46d6e14c2dcbb24
Facundo-Farnes <facundo.farnes@correo.ucu.edu.uy>
2024-10-21 09:39:09 -0300

    Ejercicio Resuelto

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,7 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+
+
+        public void PrintRecipe(ITextProvider recipe)
         {
             Console.WriteLine(recipe.GetTextToPrint());
         }
